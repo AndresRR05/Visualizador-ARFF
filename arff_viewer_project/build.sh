4d72b851a81caa8a6b7b2e9c43e922dd739bc187
Andres Rosales Ruiz <andresrosalesruiz2@gmail.com>
2025-10-27 23:41:42 -0600

    Forzar instalacion de requirements en build.sh

--- arff_viewer_project/build.sh
+++ arff_viewer_project/build.sh
@@ -2,5 +2,9 @@
 # Exit on error
 set -o errexit
 
+# Añadimos esta línea para forzar la instalación
+pip install -r requirements.txt
+
+# Comandos de Django
 python manage.py collectstatic --no-input
 python manage.py migrate
